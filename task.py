7c73b425824a4c4d504c41545c7354b3a80e69f8
GugaBogdan <163143068+GugaBogdan@users.noreply.github.com>
2024-05-07 21:19:17 +0800

    Add files via upload
    Rock, paper, scissors game

---
+++ task.py
@@ -0,0 +1,24 @@
+import random
+
+rps = ['rock', 'paper', 'scissors']
+
+player1 = random.choice(rps)
+print(f"Janym Manin gave: {player1}")
+
+player2 = random.choice(rps)
+print(f"Guvanya gave: {player2}")
+
+if player1 == "rock" and player2 == "scissors":
+    print("Winner is Janym Manin!")
+elif player1 == "scissors" and player2 == "paper":
+    print("Winner is Janym Manin!")
+elif player1 == "paper" and player2 == "rock":
+    print("Winner is Janym Manin!")
+elif player1 == "rock" and player2 == "paper":
+    print("Winner is Guvanya")
+elif player1 == "paper" and player2 == "scissors":
+    print("Winner is Guvanya")
+elif player1 == "scissors" and player2 == "rock":
+    print("Winner is Guvanya")
+else:
+    print("Tie! Run again.")
