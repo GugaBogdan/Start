c583a78408f0deb083b2a4d66c2054a42f78ebdc
GugaBogdan <163143068+GugaBogdan@users.noreply.github.com>
2024-05-13 15:59:39 +0800

    Add files via upload

---
+++ practice_tasks.py
@@ -0,0 +1,9 @@
+#https://dmoj.ca/problem/wc15c2j1
+
+far = int(input("Enter your far: "))
+far_word = 'far, ' * (far - 1)
+far_wo_comma = "far"
+
+print("A long time ago in a galaxy", far_word + far_wo_comma + " away...")
+
+
